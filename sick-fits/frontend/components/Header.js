b20538a9f82489db0fc29660636c88a9e72c42d1
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-04-22 00:13:47 +0200

    Made main nav & nprogress added & fixed style loading

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
 import Nav from "./Nav";
 import Link from "next/link";
 import styled from "styled-components";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -36,7 +48,7 @@ const StyledHeader = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 1px solid ${props => props.theme.lightGrey};
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `;
 
